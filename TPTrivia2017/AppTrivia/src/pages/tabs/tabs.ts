a83446000df1877ede4b479bd9ea7db1bbbef5af
Fernando <frivara_mgs@hotmail.com>
2017-04-15 16:05:22 -0300

    Revert "Revert "commit2""
    This reverts commit c3b4a23adcb3eb0b089f4c28fce22802c958f8bb.

--- TPTrivia2017/AppTrivia/src/pages/tabs/tabs.ts
+++ TPTrivia2017/AppTrivia/src/pages/tabs/tabs.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
+import { InfoPage } from '../info/info';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { TriviaPage } from '../trivia/trivia';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab1Root = TriviaPage;
+  tab2Root = InfoPage;
   tab3Root = ContactPage;
 
   constructor() {
--- TPTrivia2017/AppTrivia/src/pages/trivia/trivia.ts
+++ TPTrivia2017/AppTrivia/src/pages/trivia/trivia.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-trivia',
+  templateUrl: 'trivia.html'
 })
-export class HomePage {
+export class TriviaPage {
 
   constructor(public navCtrl: NavController) {
 
   }
+
+
 
 }
