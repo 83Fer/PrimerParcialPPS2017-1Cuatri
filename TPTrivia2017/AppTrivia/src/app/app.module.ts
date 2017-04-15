15d0d84c104baf701c7ef91d30459c52820c2952
Fernando <frivara_mgs@hotmail.com>
2017-04-15 15:51:25 -0300

    commit2

--- TPTrivia2017/AppTrivia/src/app/app.module.ts
+++ TPTrivia2017/AppTrivia/src/app/app.module.ts
@@ -1,35 +1,55 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+//Dependencias para usar Firebase
+import {AngularFireModule} from 'angularfire2'
+
+
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+import { InfoPage } from '../pages/info/info';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { TriviaPage } from '../pages/trivia/trivia';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+//Variable de configuracion firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyD9IjBzIQR-Z_q_jIs-qIP_PcCZY89fTt4",
+    authDomain: "triviadb-a0e25.firebaseapp.com",
+    databaseURL: "https://triviadb-a0e25.firebaseio.com",
+    projectId: "triviadb-a0e25",
+    storageBucket: "triviadb-a0e25.appspot.com",
+    messagingSenderId: "923926838804"
+}
+
+
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    InfoPage,
     ContactPage,
-    HomePage,
-    TabsPage
+    TriviaPage,
+    TabsPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)//Añadimos la importacion del firebase
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    InfoPage,
     ContactPage,
-    HomePage,
-    TabsPage
+    TriviaPage,
+    TabsPage, 
+    LoginPage
   ],
   providers: [
     StatusBar,
